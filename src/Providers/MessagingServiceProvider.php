137c5c507f4087720bdb0eb8ebc50cb6de16d4bd
Daniel Klabbers <daniel@productip.com>
2016-11-15 14:47:08 +0100

    first commit

---
+++ src/Providers/MessagingServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Flagrow\Messaging\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class MessagingServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+}
