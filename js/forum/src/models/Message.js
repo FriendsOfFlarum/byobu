19193652a18b2d35b19384e5838c5368edf267b1
Daniel Klabbers <daniel@productip.com>
2016-11-15 16:46:59 +0100

    added model

--- js/forum/src/models/Message.js
+++ js/forum/src/models/Message.js
@@ -6,7 +6,7 @@ class Message extends Model {
 Object.assign(Message.prototype, {
     content: Model.attribute('content'),
     from: Model.hasOne('from'),
-    sendAt: Model.attribute('sendAt', Model.transformDate),
+    sendAt: Model.attribute('createdAt', Model.transformDate),
     readAt: Model.attribute('readAt', Model.transformDate),
 });
 
---
+++ src/Models/Message.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Flagrow\Messaging\Models;
+
+use Carbon\Carbon;
+use Flarum\Core\User;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property text $content
+ * @property int $to_id
+ * @property int $from_id
+ * @property User $to
+ * @property User $from
+ * @property Carbon $created_at
+ * @property Carbon $read_at
+ * @property Carbon $updated_at
+ */
+class Message extends Model
+{
+
+    public function to()
+    {
+        return $this->belongsTo(User::class, 'to_id');
+    }
+
+    public function from()
+    {
+        return $this->belongsTo(User::class, 'from_id');
+    }
+}
