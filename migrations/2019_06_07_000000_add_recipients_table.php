7e0900fbd792f6a9b5306cb03cd5f714f5b9219f
Daniël Klabbers <daniel@klabbers.email>
2019-07-08 10:43:12 +0200

    absolute fix for check

--- migrations/2019_06_07_000000_add_recipients_table.php
+++ migrations/2019_06_07_000000_add_recipients_table.php
@@ -17,34 +17,29 @@ return [
             $connection->table('discussions')
                 ->whereIn('id', collect($ids)->pluck('discussion_id')->all())
                 ->update(['is_private' => 1]);
+        } else {
+            $schema->create('recipients', function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->integer('discussion_id')->unsigned()->nullable();
+                $table->integer('user_id')->unsigned();
+                $table->integer('group_id')->unsigned()->nullable();
+                $table->timestamps();
+                $table->timestamp('removed_at')->nullable();
+
+                $table->foreign('discussion_id')
+                    ->references('id')
+                    ->on('discussions')
+                    ->onDelete('cascade');
+                $table->foreign('user_id')
+                    ->references('id')
+                    ->on('users')
+                    ->onDelete('cascade');
+                $table->foreign('group_id')
+                    ->references('id')
+                    ->on('groups')
+                    ->onDelete('cascade');
+            });
         }
-
-        // stop if migrating from flagrow/byobu
-        if ($schema->getConnection()->table('migrations')->whereExtension('flagrow-byobu')->exists()) {
-            return;
-        }
-
-        $schema->create('recipients', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->integer('discussion_id')->unsigned()->nullable();
-            $table->integer('user_id')->unsigned();
-            $table->integer('group_id')->unsigned()->nullable();
-            $table->timestamps();
-            $table->timestamp('removed_at')->nullable();
-
-            $table->foreign('discussion_id')
-                ->references('id')
-                ->on('discussions')
-                ->onDelete('cascade');
-            $table->foreign('user_id')
-                ->references('id')
-                ->on('users')
-                ->onDelete('cascade');
-            $table->foreign('group_id')
-                ->references('id')
-                ->on('groups')
-                ->onDelete('cascade');
-        });
     },
     'down' => function (Builder $schema) {
         $schema->drop('recipients');
