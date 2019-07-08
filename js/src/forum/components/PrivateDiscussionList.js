429cc0c14149c5dada81d8c1b02d238aff7a7b2f
Daniël Klabbers <daniel@klabbers.email>
2019-07-08 22:30:57 +0200

    fixed caching of the byobu tab for user

--- js/src/forum/components/PrivateDiscussionList.js
+++ js/src/forum/components/PrivateDiscussionList.js
@@ -2,24 +2,4 @@ import DiscussionList from 'flarum/components/DiscussionList';
 
 export default class PrivateDiscussionList extends DiscussionList
 {
-    /**
-     * Load a new page of discussion results.
-     *
-     * @param {Integer} offset The index to start the page at.
-     * @return {Promise}
-     */
-    loadResults(offset) {
-        const preloadedDiscussions = app.preloadedDocument();
-
-        if (preloadedDiscussions) {
-            return m.deferred().resolve(preloadedDiscussions).promise;
-        }
-
-        const params = this.requestParams();
-        params.q = 'q=is:private';
-        params.page = {offset};
-        params.include = params.include.join(',');
-
-        return app.store.find('discussions', params);
-    }
 }
--- js/src/forum/components/PrivateDiscussionsUserPage.js
+++ js/src/forum/components/PrivateDiscussionsUserPage.js
@@ -1,22 +1,26 @@
 import UserPage from 'flarum/components/UserPage';
-import DiscussionList from 'flarum/components/DiscussionList';
+import PrivateDiscussionList from './PrivateDiscussionList';
 
 export default class PrivateDiscussionsUserPage extends UserPage {
     init() {
         super.init();
 
         this.loadUser(m.route.param('username'));
+
+        this.list = new PrivateDiscussionList({
+            params: {
+                q: `byobu:${this.user.username()} is:private`,
+                sort: 'newest'
+            }
+        });
+
+        this.list.refresh();
     }
 
     content() {
         return (
             <div className="DiscussionsUserPage">
-                {DiscussionList.component({
-                    params: {
-                        q: `byobu:${this.user.username()} is:private`,
-                        sort: 'newest'
-                    }
-                })}
+                {this.list.render()}
             </div>
         );
     }
