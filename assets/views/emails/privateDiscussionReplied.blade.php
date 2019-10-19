9972f2c304fdae3d9108546d7e578eea904eba23
Ian Morland <ianm@giffgaff.co.uk>
2019-10-19 20:52:35 +0100

    Include post content in notif email

--- assets/views/emails/privateDiscussionReplied.blade.php
+++ assets/views/emails/privateDiscussionReplied.blade.php
@@ -2,4 +2,8 @@ Hey {{ $user->username }}!
 
 {{ $blueprint->post->user->username }} replied in a private discussion with you as recipient, titled '{{ $blueprint->post->discussion->title }}''.
 
-View it here: {{ app()->url() }}/d/{{ $blueprint->post->discussion->id }}/{{ $blueprint->post->number }}
+View it here: {{ app()->url() }}/d/{{ $blueprint->post->discussion->id }}-{{ $blueprint->post->discussion->slug }}/{{ $blueprint->post->number }}
+
+---
+
+{!! $blueprint->post->content !!}
