fe0d3e2183be405fa6ae846be36acb8154baed4b
Ian Morland <ianm@giffgaff.co.uk>
2020-02-16 10:38:33 +0000

    Add sort options to private discussion page

--- js/src/forum/components/PrivateDiscussionsUserPage.js
+++ js/src/forum/components/PrivateDiscussionsUserPage.js
@@ -1,11 +1,15 @@
 import UserPage from 'flarum/components/UserPage';
 import PrivateDiscussionList from './PrivateDiscussionList';
+import Button from 'flarum/components/Button';
+import Dropdown from 'flarum/components/Dropdown';
+import ItemList from 'flarum/utils/ItemList';
+import listItems from 'flarum/helpers/listItems';
 
 export default class PrivateDiscussionsUserPage extends UserPage {
     init() {
         super.init();
 
-        this.loadUser(m.route.param('username'));
+        this.changeSort('latest');
     }
 
     show(user) {
@@ -13,7 +17,7 @@ export default class PrivateDiscussionsUserPage extends UserPage {
         this.list = new PrivateDiscussionList({
             params: {
                 q: `byobu:${user.username()} is:private`,
-                sort: 'newest'
+                sort: this.sort,
             }
         });
 
@@ -24,11 +28,56 @@ export default class PrivateDiscussionsUserPage extends UserPage {
         super.show(user);
     }
 
+    handleChangeSort(sort, e) {
+        e.preventDefault();
+
+        this.changeSort(sort);
+    }
+
+    changeSort(sort) {
+        this.sort = sort;
+        this.loadUser(m.route.param('username'));
+    }
+
     content() {
         return (
             <div className="DiscussionsUserPage">
+                <div className="DiscussionsUserPage-toolbar">
+                    <ul className="DiscussionsUserPage-toolbar-action"></ul>
+                    <ul className="DiscussionsUserPage-toolbar-view">{listItems(this.viewItems().toArray())}</ul>
+                </div>
                 {this.list.render()}
             </div>
         );
     }
+
+    viewItems() {
+        const items = new ItemList();
+        const sortMap = this.list.sortMap();
+    
+        const sortOptions = {};
+        for (const i in sortMap) {
+          sortOptions[i] = app.translator.trans('core.forum.index_sort.' + i + '_button');
+        }
+    
+        items.add('sort',
+          Dropdown.component({
+            buttonClassName: 'Button',
+            label: sortOptions[this.sort] || Object.keys(sortMap).map(key => sortOptions[key])[0],
+            children: Object.keys(sortOptions).map(value => {
+              const label = sortOptions[value];
+              const active = (this.sort || Object.keys(sortMap)[0]) === value;
+    
+              return Button.component({
+                children: label,
+                icon: active ? 'fas fa-check' : true,
+                onclick: this.handleChangeSort.bind(this, value),
+                active: active,
+              })
+            }),
+          })
+        );
+    
+        return items;
+      }
 }
