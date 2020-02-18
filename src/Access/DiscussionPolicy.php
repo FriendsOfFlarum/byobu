9f29671197c177aaec72c85b0abc1deb94ce7519
Ian Morland <ianm@giffgaff.co.uk>
2020-02-18 14:54:48 +0000

    Improve policy query

--- src/Access/DiscussionPolicy.php
+++ src/Access/DiscussionPolicy.php
@@ -42,27 +42,25 @@ class DiscussionPolicy extends AbstractPolicy
     public function findPrivate(User $actor, EloquentBuilder $query)
     {
         if ($actor->exists) {
-            $query->orWhereExists(function (Builder $query) use ($actor) {
-                $query->whereIn('discussions.id', function ($query) use ($actor) {
-                    $query->select('discussion_id')
-                        ->from('recipients')
-                        ->whereNull('removed_at')
-                        ->where('user_id', $actor->id)
-                        ->orWhereIn('group_id', $actor->groups->pluck('id')->all());
-                });
-
-                if (
-                    $this->extensions->isEnabled('flarum-flags') &&
-                    $actor->hasPermission('user.viewPrivateDiscussionsWhenFlagged') &&
-                    $actor->hasPermission('discussion.viewFlags')
-                ) {
-                    $query->orWhereIn('discussions.id', function ($query) {
-                        $query->select('posts.discussion_id')
-                            ->from('flags')
-                            ->leftJoin('posts', 'flags.post_id', 'posts.id');
-                    });
-                }
+            $query->orWhereIn('discussions.id', function ($query) use ($actor) {
+                $query->select('discussion_id')
+                    ->from('recipients')
+                    ->whereNull('removed_at')
+                    ->where('user_id', $actor->id)
+                    ->orWhereIn('group_id', $actor->groups->pluck('id')->all());
             });
+
+            if (
+                $this->extensions->isEnabled('flarum-flags') &&
+                $actor->hasPermission('user.viewPrivateDiscussionsWhenFlagged') &&
+                $actor->hasPermission('discussion.viewFlags')
+            ) {
+                $query->orWhereIn('discussions.id', function ($query) {
+                    $query->select('posts.discussion_id')
+                        ->from('flags')
+                        ->leftJoin('posts', 'flags.post_id', 'posts.id');
+                });
+            };
         }
     }
 
