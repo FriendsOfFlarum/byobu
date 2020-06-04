69a94825a2581c4b5c41cca8efa01f9b8701eab5
Ian Morland <ianm@giffgaff.co.uk>
2020-06-04 22:27:58 +0100

    Should fix dupe notifications #114

--- src/Listeners/QueueNotificationJobs.php
+++ src/Listeners/QueueNotificationJobs.php
@@ -18,6 +18,7 @@ use FoF\Byobu\Events\DiscussionRecipientRemovedSelf;
 use FoF\Byobu\Events\DiscussionRecipientsChanged;
 use FoF\Byobu\Jobs;
 use Illuminate\Events\Dispatcher;
+use s9e\TextFormatter\Utils;
 
 class QueueNotificationJobs
 {
@@ -41,6 +42,12 @@ class QueueNotificationJobs
     {
         // stop the notification from firing when events such as flarum/likes or fof/reactions re-save the post.
         if ($event->post->exists) {
+            return;
+        }
+
+        // If the post content contains a postmention, don't notify here, we will assume some other event will handle it.
+        $postMentions = Utils::getAttributeValues($event->post->parsedContent, 'POSTMENTION', 'id');
+        if ($postMentions > 0) {
             return;
         }
 
