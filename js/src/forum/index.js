b93284c21bb5415d4921fe918b924d7abc05ad2e
Daniël Klabbers <daniel@klabbers.email>
2020-11-23 21:05:09 +0100

    clean up

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -9,29 +9,4 @@ app.initializers.add('fof-byobu', function (app) {
 
     pages(app);
     notifications(app);
-
-    //
-    // // Add notification preferences.
-    // extend(NotificationGrid.prototype, 'notificationTypes', function (items) {
-    //     items.add('byobuPrivateDiscussionCreated', {
-    //         name: 'byobuPrivateDiscussionCreated',
-    //         icon: 'fas fa-map',
-    //         label: app.translator.trans('fof-byobu.forum.notifications.pd_label'),
-    //     });
-    //     items.add('byobuPrivateDiscussionReplied', {
-    //         name: 'byobuPrivateDiscussionReplied',
-    //         icon: 'fas fa-map',
-    //         label: app.translator.trans('fof-byobu.forum.notifications.pd_reply_label'),
-    //     });
-    //     items.add('byobuPrivateDiscussionAdded', {
-    //         name: 'byobuPrivateDiscussionAdded',
-    //         icon: 'fas fa-map',
-    //         label: app.translator.trans('fof-byobu.forum.notifications.pd_added_label'),
-    //     });
-    //     items.add('byobuRecipientRemoved', {
-    //         name: 'byobuRecipientRemoved',
-    //         icon: 'fas fa-map',
-    //         label: app.translator.trans('fof-byobu.forum.notifications.pd_user_left_label'),
-    //     });
-    // });
 });
