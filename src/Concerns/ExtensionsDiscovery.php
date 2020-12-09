bcfc73f4be777de0162b37cd1ca47ab250296949
luceos <luceos@users.noreply.github.com>
2020-12-09 18:11:10 +0000

    Apply fixes from StyleCI

--- src/Concerns/ExtensionsDiscovery.php
+++ src/Concerns/ExtensionsDiscovery.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of fof/byobu.
+ *
+ * Copyright (c) 2019 FriendsOfFlarum.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace FoF\Byobu\Concerns;
 
 use Flarum\Extension\ExtensionManager;
@@ -8,6 +17,7 @@ trait ExtensionsDiscovery
 {
     /**
      * @param string $extension ; extension id (fof-byobu)
+     *
      * @return bool
      */
     public function extensionIsEnabled(string $extension): bool
