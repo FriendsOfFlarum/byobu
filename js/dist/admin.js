55b92d4eb1cbe726d52af780856ecd9ee93d078f
Daniël Klabbers <daniel@klabbers.email>
2020-12-15 21:10:47 +0100

    more changes for b15

--- js/dist/admin.js
+++ js/dist/admin.js
@@ -1,2 +1,181 @@
-module.exports=function(e){var t={};function r(i){if(t[i])return t[i].exports;var s=t[i]={i:i,l:!1,exports:{}};return e[i].call(s.exports,s,s.exports,r),s.l=!0,s.exports}return r.m=e,r.c=t,r.d=function(e,t,i){r.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:i})},r.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},r.t=function(e,t){if(1&t&&(e=r(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var i=Object.create(null);if(r.r(i),Object.defineProperty(i,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var s in e)r.d(i,s,function(t){return e[t]}.bind(null,s));return i},r.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(t,"a",t),t},r.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},r.p="",r(r.s=41)}({0:function(e,t){e.exports=flarum.core.compat.extend},21:function(e,t){e.exports=flarum.core.compat["components/PermissionGrid"]},39:function(e,t){e.exports=flarum.core.compat["core/models/User"]},41:function(e,t,r){"use strict";r.r(t);var i=r(39),s=r.n(i),n=r(0),o=r(21),a=r.n(o),u=function(){Object(n.extend)(a.a.prototype,"startItems",(function(e){e.add("startPrivateUsers",{icon:"far fa-map",label:app.translator.trans("fof-byobu.admin.permission.create_private_discussions_with_users"),permission:"discussion.startPrivateDiscussionWithUsers"},95),e.add("startPrivateGroups",{icon:"far fa-map",label:app.translator.trans("fof-byobu.admin.permission.create_private_discussions_with_groups"),permission:"discussion.startPrivateDiscussionWithGroups"},95),e.add("startPrivateBlockers",{icon:"far fa-map",label:app.translator.trans("fof-byobu.admin.permission.create_private_discussions_with_blocking_users"),permission:"startPrivateDiscussionWithBlockers"},95)})),Object(n.extend)(a.a.prototype,"moderateItems",(function(e){e.add("editUserRecipients",{icon:"far fa-map",label:app.translator.trans("fof-byobu.admin.permission.edit_user_recipients"),permission:"discussion.editUserRecipients"},95),e.add("editGroupRecipients",{icon:"far fa-map",label:app.translator.trans("fof-byobu.admin.permission.edit_group_recipients"),permission:"discussion.editGroupRecipients"},95),e.add("actor-can-view-private-discussions-when-flagged",{icon:"fas fa-flag",label:app.translator.trans("fof-byobu.admin.permission.view_private_discussions-when-flagged"),permission:"user.viewPrivateDiscussionsWhenFlagged"},95)}))};app.initializers.add("fof-byobu",(function(e){e.store.models.recipients=s.a,u()}))}});
+module.exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./admin.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./admin.js":
+/*!******************!*\
+  !*** ./admin.js ***!
+  \******************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _src_admin__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./src/admin */ "./src/admin/index.js");
+/* empty/unused harmony star reexport */
+
+/***/ }),
+
+/***/ "./src/admin/addPrivateDiscussionPermission.js":
+/*!*****************************************************!*\
+  !*** ./src/admin/addPrivateDiscussionPermission.js ***!
+  \*****************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = (function (app) {
+  app.extensionData["for"]('fof-byobu').registerPermission({
+    icon: 'far fa-map',
+    label: app.translator.trans('fof-byobu.admin.permission.create_private_discussions_with_users'),
+    permission: 'discussion.startPrivateDiscussionWithUsers'
+  }, 'start', 95);
+  app.extensionData["for"]('fof-byobu').registerPermission({
+    icon: 'far fa-map',
+    label: app.translator.trans('fof-byobu.admin.permission.create_private_discussions_with_groups'),
+    permission: 'discussion.startPrivateDiscussionWithGroups'
+  }, 'start', 95);
+  app.extensionData["for"]('fof-byobu').registerPermission({
+    icon: 'far fa-map',
+    label: app.translator.trans('fof-byobu.admin.permission.create_private_discussions_with_blocking_users'),
+    permission: 'startPrivateDiscussionWithBlockers'
+  }, 'start', 95);
+  app.extensionData["for"]('fof-byobu').registerPermission({
+    icon: 'far fa-map',
+    label: app.translator.trans('fof-byobu.admin.permission.edit_user_recipients'),
+    permission: 'discussion.editUserRecipients'
+  }, 'moderate', 95);
+  app.extensionData["for"]('fof-byobu').registerPermission({
+    icon: 'far fa-map',
+    label: app.translator.trans('fof-byobu.admin.permission.edit_group_recipients'),
+    permission: 'discussion.editGroupRecipients'
+  }, 'moderate', 95);
+  app.extensionData["for"]('fof-byobu').registerPermission({
+    icon: 'fas fa-flag',
+    label: app.translator.trans('fof-byobu.admin.permission.view_private_discussions-when-flagged'),
+    permission: 'user.viewPrivateDiscussionsWhenFlagged'
+  }, 'moderate', 95);
+});
+
+/***/ }),
+
+/***/ "./src/admin/index.js":
+/*!****************************!*\
+  !*** ./src/admin/index.js ***!
+  \****************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var flarum_core_models_User__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! flarum/core/models/User */ "flarum/core/models/User");
+/* harmony import */ var flarum_core_models_User__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(flarum_core_models_User__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _addPrivateDiscussionPermission__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./addPrivateDiscussionPermission */ "./src/admin/addPrivateDiscussionPermission.js");
+
+
+app.initializers.add('fof-byobu', function (app) {
+  app.store.models.recipients = flarum_core_models_User__WEBPACK_IMPORTED_MODULE_0___default.a;
+  Object(_addPrivateDiscussionPermission__WEBPACK_IMPORTED_MODULE_1__["default"])(app);
+});
+
+/***/ }),
+
+/***/ "flarum/core/models/User":
+/*!*********************************************************!*\
+  !*** external "flarum.core.compat['core/models/User']" ***!
+  \*********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = flarum.core.compat['core/models/User'];
+
+/***/ })
+
+/******/ });
 //# sourceMappingURL=admin.js.map
--- js/src/admin/addPrivateDiscussionPermission.js
+++ js/src/admin/addPrivateDiscussionPermission.js
@@ -1,63 +1,49 @@
-import { extend } from 'flarum/extend';
-import PermissionGrid from 'flarum/components/PermissionGrid';
+export default function (app) {
+    app.extensionData
+        .for('fof-byobu')
+        .registerPermission({
+            icon: 'far fa-map',
+            label: app.translator.trans('fof-byobu.admin.permission.create_private_discussions_with_users'),
+            permission: 'discussion.startPrivateDiscussionWithUsers',
+        }, 'start', 95);
 
-export default function () {
-    extend(PermissionGrid.prototype, 'startItems', (items) => {
-        items.add(
-            'startPrivateUsers',
-            {
-                icon: 'far fa-map',
-                label: app.translator.trans('fof-byobu.admin.permission.create_private_discussions_with_users'),
-                permission: 'discussion.startPrivateDiscussionWithUsers',
-            },
-            95
-        );
-        items.add(
-            'startPrivateGroups',
-            {
-                icon: 'far fa-map',
-                label: app.translator.trans('fof-byobu.admin.permission.create_private_discussions_with_groups'),
-                permission: 'discussion.startPrivateDiscussionWithGroups',
-            },
-            95
-        );
-        items.add(
-            'startPrivateBlockers',
-            {
-                icon: 'far fa-map',
-                label: app.translator.trans('fof-byobu.admin.permission.create_private_discussions_with_blocking_users'),
-                permission: 'startPrivateDiscussionWithBlockers',
-            },
-            95
-        );
-    });
-    extend(PermissionGrid.prototype, 'moderateItems', (items) => {
-        items.add(
-            'editUserRecipients',
-            {
-                icon: 'far fa-map',
-                label: app.translator.trans('fof-byobu.admin.permission.edit_user_recipients'),
-                permission: 'discussion.editUserRecipients',
-            },
-            95
-        );
-        items.add(
-            'editGroupRecipients',
-            {
-                icon: 'far fa-map',
-                label: app.translator.trans('fof-byobu.admin.permission.edit_group_recipients'),
-                permission: 'discussion.editGroupRecipients',
-            },
-            95
-        );
-        items.add(
-            'actor-can-view-private-discussions-when-flagged',
-            {
-                icon: 'fas fa-flag',
-                label: app.translator.trans('fof-byobu.admin.permission.view_private_discussions-when-flagged'),
-                permission: 'user.viewPrivateDiscussionsWhenFlagged',
-            },
-            95
-        );
-    });
+    app.extensionData
+        .for('fof-byobu')
+        .registerPermission({
+            icon: 'far fa-map',
+            label: app.translator.trans('fof-byobu.admin.permission.create_private_discussions_with_groups'),
+            permission: 'discussion.startPrivateDiscussionWithGroups',
+        }, 'start', 95);
+
+    app.extensionData
+        .for('fof-byobu')
+        .registerPermission({
+            icon: 'far fa-map',
+            label: app.translator.trans('fof-byobu.admin.permission.create_private_discussions_with_blocking_users'),
+            permission: 'startPrivateDiscussionWithBlockers',
+        }, 'start', 95);
+
+    app.extensionData
+        .for('fof-byobu')
+        .registerPermission({
+            icon: 'far fa-map',
+            label: app.translator.trans('fof-byobu.admin.permission.edit_user_recipients'),
+            permission: 'discussion.editUserRecipients',
+        }, 'moderate', 95);
+
+    app.extensionData
+        .for('fof-byobu')
+        .registerPermission({
+            icon: 'far fa-map',
+            label: app.translator.trans('fof-byobu.admin.permission.edit_group_recipients'),
+            permission: 'discussion.editGroupRecipients',
+        }, 'moderate', 95);
+
+    app.extensionData
+        .for('fof-byobu')
+        .registerPermission({
+            icon: 'fas fa-flag',
+            label: app.translator.trans('fof-byobu.admin.permission.view_private_discussions-when-flagged'),
+            permission: 'user.viewPrivateDiscussionsWhenFlagged',
+        }, 'moderate', 95);
 }
