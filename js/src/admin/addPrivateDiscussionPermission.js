03b2908384ef0b1800789b65225f9665d2759580
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-12-15 21:38:25 -0500

    Condense permission registration into one call

--- js/src/admin/addPrivateDiscussionPermission.js
+++ js/src/admin/addPrivateDiscussionPermission.js
@@ -5,42 +5,27 @@ export default function (app) {
             icon: 'far fa-map',
             label: app.translator.trans('fof-byobu.admin.permission.create_private_discussions_with_users'),
             permission: 'discussion.startPrivateDiscussionWithUsers',
-        }, 'start', 95);
-
-    app.extensionData
-        .for('fof-byobu')
+        }, 'start', 95)
         .registerPermission({
             icon: 'far fa-map',
             label: app.translator.trans('fof-byobu.admin.permission.create_private_discussions_with_groups'),
             permission: 'discussion.startPrivateDiscussionWithGroups',
-        }, 'start', 95);
-
-    app.extensionData
-        .for('fof-byobu')
+        }, 'start', 95)
         .registerPermission({
             icon: 'far fa-map',
             label: app.translator.trans('fof-byobu.admin.permission.create_private_discussions_with_blocking_users'),
             permission: 'startPrivateDiscussionWithBlockers',
-        }, 'start', 95);
-
-    app.extensionData
-        .for('fof-byobu')
+        }, 'start', 95)
         .registerPermission({
             icon: 'far fa-map',
             label: app.translator.trans('fof-byobu.admin.permission.edit_user_recipients'),
             permission: 'discussion.editUserRecipients',
-        }, 'moderate', 95);
-
-    app.extensionData
-        .for('fof-byobu')
+        }, 'moderate', 95)
         .registerPermission({
             icon: 'far fa-map',
             label: app.translator.trans('fof-byobu.admin.permission.edit_group_recipients'),
             permission: 'discussion.editGroupRecipients',
-        }, 'moderate', 95);
-
-    app.extensionData
-        .for('fof-byobu')
+        }, 'moderate', 95)
         .registerPermission({
             icon: 'fas fa-flag',
             label: app.translator.trans('fof-byobu.admin.permission.view_private_discussions-when-flagged'),
