62bf4cabdc57d8e00d190e4b0c3b721b7677e9bd
Ian Morland <ian@morland.me>
2020-12-19 11:56:50 +0000

    Don't notify private discussion author that they started the discussion

--- src/Jobs/SendNotificationWhenPrivateDiscussionStarted.php
+++ src/Jobs/SendNotificationWhenPrivateDiscussionStarted.php
@@ -51,7 +51,9 @@ class SendNotificationWhenPrivateDiscussionStarted implements ShouldQueue
 
     public function handle(NotificationSyncer $notifications)
     {
-        $userRecipients = $this->newUsers;
+        $userRecipients = $this->newUsers->reject(function ($user) {
+            return $user->id === $this->discussion->user->id;
+        });
 
         $groupRecipientUsers = User::leftJoin('group_user', 'users.id', 'group_user.user_id')
             ->whereIn('group_user.group_id', $this->newGroups)
