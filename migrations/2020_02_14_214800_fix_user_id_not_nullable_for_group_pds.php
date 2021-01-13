1f98928e12a93ec209b80b38cd5cc25bcd8e5b63
Ian Morland <imorland@users.noreply.github.com>
2021-01-13 20:52:45 +0000

    Apply fixes from StyleCI

--- migrations/2020_02_14_214800_fix_user_id_not_nullable_for_group_pds.php
+++ migrations/2020_02_14_214800_fix_user_id_not_nullable_for_group_pds.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of fof/byobu.
  *
- * Copyright (c) 2019 FriendsOfFlarum.
+ * Copyright (c) 2019 - 2021 FriendsOfFlarum.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
