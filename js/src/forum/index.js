6eaff5af9100b003318d3f454f25e270166bea8e
Ian Morland <ian@morland.me>
2021-01-15 21:50:56 +0000

    Export some

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -3,6 +3,9 @@ import extend from './extend';
 import pages from './pages';
 import notifications from './notifications';
 
+export * from './modals';
+export * from './pages/discussions';
+
 app.initializers.add('fof-byobu', function (app) {
     events(app);
     extend(app);
