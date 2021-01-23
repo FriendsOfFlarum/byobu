036893e1cb396c3cc9040f6c734809b3df424b76
Ian Morland <ian@morland.me>
2021-01-23 17:10:50 +0000

    Drop tags from pre-existing private discussions

---
+++ migrations/2021_01_23_000000_drop_tags_from_old_private_discussions.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of fof/byobu.
+ *
+ * Copyright (c) 2019 - 2021 FriendsOfFlarum.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\Builder;
+
+return [
+    'up' => function (Builder $schema) {
+        $connection = $schema->getConnection();
+        $prefix = $connection->getTablePrefix();
+
+        $connection->statement("DELETE FROM {$prefix}discussion_tag WHERE discussion_id IN (SELECT DISTINCT(discussion_id) FROM {$prefix}recipients)");
+    },
+    'down' => function (Builder $schema) {
+        //
+    },
+];
