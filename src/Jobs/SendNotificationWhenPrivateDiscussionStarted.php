515374d9775fddfe9f94f884f5fe02f083552e75
Daniel Klabbers <daniel+git@klabbers.email>
2021-04-19 12:58:46 +0200

    fix array issue with groups notificationsg

--- src/Jobs/SendNotificationWhenPrivateDiscussionStarted.php
+++ src/Jobs/SendNotificationWhenPrivateDiscussionStarted.php
@@ -56,7 +56,7 @@ class SendNotificationWhenPrivateDiscussionStarted implements ShouldQueue
         });
 
         $groupRecipientUsers = User::leftJoin('group_user', 'users.id', 'group_user.user_id')
-            ->whereIn('group_user.group_id', $this->newGroups)
+            ->whereIn('group_user.group_id', $this->newGroups->pluck('id'))
             ->whereNotIn('users.id', [$this->discussion->user_id])
             ->get();
 
