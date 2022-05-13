ac5fdff10dceccc01ea57f764c44d0270554205b
Clark Winkelmann <clark.winkelmann@gmail.com>
2022-05-13 22:40:09 +0200

    Fix index down migration (#172)
    * Fix index down migration #160
    
    * Apply fixes from StyleCI
    
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- migrations/2021_01_13_000000_unified_index_index.php
+++ migrations/2021_01_13_000000_unified_index_index.php
@@ -11,6 +11,7 @@
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Schema\Builder;
+use Illuminate\Support\Arr;
 
 return [
     'up' => function (Builder $schema) {
@@ -19,7 +20,19 @@ return [
         });
     },
     'down' => function (Builder $schema) {
-        $schema->table('users', function (Blueprint $table) {
+        $schema->table('users', function (Blueprint $table) use ($schema) {
+            $connection = $schema->getConnection();
+            $schemaManager = $connection->getDoctrineSchemaManager();
+            $existingIndexes = $schemaManager->listTableIndexes($table->getTable());
+
+            // If migration 2021_04_21_000000_drop_users_unified_index_column has been applied, this index already no longer exists
+            // Unfortunately we can't call the protected createIndexName method so we have to build the index name manually
+            $indexName = strtolower(str_replace(['-', '.'], '_', $connection->getTablePrefix().$table->getTable()).'_unified_index_with_byobu_index');
+
+            if (!Arr::exists($existingIndexes, $indexName)) {
+                return;
+            }
+
             // Use array syntax so the blueprint "guesses" the full index name
             $table->dropIndex(['unified_index_with_byobu']);
         });
