4c22ecfa78fc13297884d22a620f8021f9024900
Rafał Całka <calka.rafal@gmail.com>
2022-07-22 10:22:10 +0200

    Hide the new private discussion button if the recipient blocks it

---
+++ js/src/forum/helpers/canStartPrivateDiscussion.js
@@ -0,0 +1,10 @@
+import app from 'flarum/forum/app';
+
+export default (recipient) => {
+  return (
+    app.session.user &&
+    app.session.user.id() !== recipient.id() &&
+    app.forum.attribute('canStartPrivateDiscussion') &&
+    (recipient.blocksPd() === false || (app.forum.attribute('canStartPrivateDiscussionWithBlockers') && recipient.cannotBeDirectMessaged()))
+  );
+};
