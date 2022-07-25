28393a98e29e460e49e33bccc4f1e7d91f5e878d
Rafał Całka <calka.rafal@gmail.com>
2022-07-25 09:17:04 +0200

    Shorter version of the function

--- js/src/forum/helpers/canStartPrivateDiscussion.js
+++ js/src/forum/helpers/canStartPrivateDiscussion.js
@@ -1,10 +1,7 @@
 import app from 'flarum/forum/app';
 
-export default (recipient) => {
-  return (
-    app.session.user &&
-    app.session.user.id() !== recipient.id() &&
-    app.forum.attribute('canStartPrivateDiscussion') &&
-    (recipient.blocksPd() === false || (app.forum.attribute('canStartPrivateDiscussionWithBlockers') && recipient.cannotBeDirectMessaged()))
-  );
-};
+export default (recipient) =>
+  app.session.user &&
+  app.session.user.id() !== recipient.id() &&
+  app.forum.attribute('canStartPrivateDiscussion') &&
+  (recipient.blocksPd() === false || (app.forum.attribute('canStartPrivateDiscussionWithBlockers') && recipient.cannotBeDirectMessaged()));
