b6330d8679c512672979451173393955408d2d17
Rafał Całka <calka.rafal@gmail.com>
2022-07-31 22:10:55 +0200

    Use named function
    Co-authored-by: David Wheatley <david@davwheat.dev>

--- js/src/forum/helpers/canStartPrivateDiscussion.js
+++ js/src/forum/helpers/canStartPrivateDiscussion.js
@@ -1,7 +1,8 @@
 import app from 'flarum/forum/app';
 
-export default (recipient) =>
-  app.session.user &&
-  app.session.user.id() !== recipient.id() &&
-  app.forum.attribute('canStartPrivateDiscussion') &&
-  (recipient.blocksPd() === false || (app.forum.attribute('canStartPrivateDiscussionWithBlockers') && recipient.cannotBeDirectMessaged()));
+export default function canStartPrivateDiscussion(recipient) {
+  return app.session.user &&
+    app.session.user.id() !== recipient.id() &&
+    app.forum.attribute('canStartPrivateDiscussion') &&
+    (!recipient.blocksPd() || (app.forum.attribute('canStartPrivateDiscussionWithBlockers') && recipient.cannotBeDirectMessaged())); 
+}
