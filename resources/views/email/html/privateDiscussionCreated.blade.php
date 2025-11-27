6a539dd15b9782b863364360393c6191ea958cab
IanM <ian@morland.me>
2025-11-27 18:24:21 +0000

    chore(2.0): notification emails now require both plain text and HTML views
    You now need to provide both plain text and HTML views for notification emails.

--- resources/views/email/html/privateDiscussionCreated.blade.php
+++ resources/views/email/html/privateDiscussionCreated.blade.php
@@ -1,6 +1,12 @@
-{!! $translator->trans('fof-byobu.email.body.private_discussion_created', [
+<x-mail::html.notification>
+    <x-slot:body>
+        {!! $translator->trans('fof-byobu.email.body.private_discussion_created', [
     '{recipient_display_name}' => $user->display_name,
     '{actor_display_name}' => $blueprint->discussion->user->display_name,
     '{discussion_title}' => $blueprint->discussion->title,
     '{discussion_url}' => $url->to('forum')->route('discussion', ['id' => $blueprint->discussion->id]),
 ]) !!}
+    </x-slot:body>
+
+    <x-slot:preview><!-- Optional content --></x-slot:preview>
+</x-mail::html.notification>
--- resources/views/email/plain/byobuRecipientRemoved.blade.php
+++ resources/views/email/plain/byobuRecipientRemoved.blade.php
@@ -1,6 +1,10 @@
-{!! $translator->trans('fof-byobu.email.body.made_public', [
+<x-mail::plain.notification>
+<x-slot:body>
+{!! $translator->trans('fof-byobu.email.body.recipient_removed', [
     '{recipient_display_name}' => $user->display_name,
-    '{actor_display_name}' => $blueprint->actor->display_name,
+    '{actor_display_name}' => $blueprint->user->display_name,
     '{discussion_title}' => $blueprint->discussion->title,
     '{discussion_url}' => $url->to('forum')->route('discussion', ['id' => $blueprint->discussion->id]),
 ]) !!}
+</x-slot:body>
+</x-mail::plain.notification>
